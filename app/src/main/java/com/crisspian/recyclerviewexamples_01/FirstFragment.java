b713b2e1f12ba7341baf0e35f6db1d5dd9f63c19
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-18 23:14:56 -0400

    M17_01

--- app/src/main/java/com/crisspian/recyclerviewexamples_01/FirstFragment.java
+++ app/src/main/java/com/crisspian/recyclerviewexamples_01/FirstFragment.java
@@ -1,21 +1,33 @@
 package com.crisspian.recyclerviewexamples_01;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.crisspian.recyclerviewexamples_01.adapter.ItemAdapter;
+import com.crisspian.recyclerviewexamples_01.databinding.FragmentFirstBinding;
+import com.crisspian.recyclerviewexamples_01.databinding.ItemListDataBinding;
 import com.crisspian.recyclerviewexamples_01.model.Item;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements ItemAdapter.PassData {
+    private FragmentFirstBinding mBinding;
+    private RecyclerView mRecyclerView;
+    private ItemAdapter mItemAdapter;
+
 
     @Override
     public View onCreateView(
@@ -23,7 +35,12 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        mBinding = FragmentFirstBinding.inflate(inflater,container,false);
+        mRecyclerView = mBinding.rvItem;
+        mItemAdapter = new ItemAdapter(returnItemList(),this);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
+        mRecyclerView.setAdapter(mItemAdapter);
+        return mBinding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -61,5 +78,11 @@ public class FirstFragment extends Fragment {
         Item item12 = new Item("Wesley Armstrong","https://unsplash.com/photos/q0wqYpyWDpc/download?force=true&w=640");
         listItem.add(item12);
         return listItem;
+    }
+
+    @Override
+    public void enviar(Item item) {
+        Toast.makeText(getContext(),item.getUrlImage(),Toast.LENGTH_SHORT).show();
+        Navigation.findNavController(mBinding.getRoot()).navigate(R.id.action_FirstFragment_to_SecondFragment);
     }
 }
--- app/src/main/java/com/crisspian/recyclerviewexamples_01/adapter/ItemAdapter.java
+++ app/src/main/java/com/crisspian/recyclerviewexamples_01/adapter/ItemAdapter.java
@@ -1,6 +1,75 @@
 package com.crisspian.recyclerviewexamples_01.adapter;
 
-public class ItemAdapter {
+import android.renderscript.ScriptGroup;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.crisspian.recyclerviewexamples_01.databinding.ItemListDataBinding;
+import com.crisspian.recyclerviewexamples_01.model.Item;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder>{
+    private List<Item> itemList;
+    private PassData callBack;
+
+    public ItemAdapter(List<Item> itemList, PassData mCallBack) {
+        this.itemList = itemList;
+        this.callBack = mCallBack;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemListDataBinding mBinding = ItemListDataBinding
+                .inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new ItemViewHolder(mBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        Item mItem = itemList.get(position);
+        holder.textView.setText(mItem.getItemDescription());
+        Glide.with(holder.itemView.getContext()).load(mItem.getUrlImage())
+                .centerCrop().into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private ImageView imageView;
+        private TextView textView;
+
+        public ItemViewHolder(@NonNull ItemListDataBinding mBinding) {
+            super(mBinding.getRoot());
+            imageView = mBinding.ivItem;
+            textView = mBinding.tvItem;
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            callBack.enviar(itemList.get(getLayoutPosition()));
+
+        }
+    }
+    public interface PassData{
+        void enviar (Item item);
+    }
+
+
 
 
 }
